1033c407f9e5e1b6df56c1f866bcf98e9d96ccad
MBOng124 <33584389+okLang32@users.noreply.github.com>
2018-08-04 19:28:01 +0800

    Added more considerations to Train Thread and Passenger thread

--- src/View/Passenger_Thread.java
+++ src/View/Passenger_Thread.java
@@ -1,5 +1,5 @@
 package View;
-
+/*  STILL LACKS CODE FOR ANIMATION  */
 public class Passenger_Thread extends Thread{
     public Passenger panel;
 
@@ -21,24 +21,33 @@ public class Passenger_Thread extends Thread{
                     e.printStackTrace();
                 }
             }else{
-                if(panel.getCurrent().getTrains().getPassengers().size() <
-                        panel.getCurrent().getTrains().getMaxCount()){
-                    panel.getCurrent().getTrains().getPassengers().add(panel);
-                    panel.setTrain(panel.getCurrent().getTrains());
-                    try {
-                        this.sleep(500);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+                if(panel.getTrain() == null){// if the passenger is not in a train
+                    if(panel.getCurrent().getTrains().getPassengers().size() <
+                            panel.getCurrent().getTrains().getMaxCount()){
+                        panel.getCurrent().getTrains().getPassengers().add(panel);
+                        panel.setTrain(panel.getCurrent().getTrains());
+                        try {
+                            this.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        panel.setVisible(false);
+                        panel.getCurrent().getTrains().getThread().notify();
+                    }else if(panel.getCurrent().getTrains().getPassengers().size() >=
+                            panel.getCurrent().getTrains().getPassengers().size()){
+                        try {
+                            this.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }else if(panel.getTrain() != null){// if the passenger is in a train
+                        // sets the passenger's current station to that of the train's
+                        panel.setCurrent(panel.getTrain().getCurrent());
+                        if(panel.getCurrent().getStationId() == panel.getEnd()){
+                            //leaves the train and terminates the thread
+                        }
                     }
-                    panel.setVisible(false);
-                }else if(panel.getCurrent().getTrains().getPassengers().size() >=
-                        panel.getCurrent().getTrains().getPassengers().size()){
-                    try {
-                        this.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }//else if()
+                }
             }
         }
     }
--- src/View/Train_Thread.java
+++ src/View/Train_Thread.java
@@ -1,8 +1,9 @@
 package View;
-
+/*  STILL LACKS CODE FOR ANIMATION  */
 public class Train_Thread extends Thread{
     public Train panel;
     private int initX, initY;
+    private Terminal terminal;
 
     public Train_Thread(Train train){
         this.panel = train;
@@ -19,15 +20,20 @@ public class Train_Thread extends Thread{
             }
             if(panel.getCurrent() instanceof Station){
                 if(panel.getPassengers().size() == panel.getMaxCount() ||
-                        panel.getCurrent().getPassengers().size() == 0){
-                    if(panel.getNext().getTrains() == null){
-                        //move train
-                    }else {
-                        try {
-                            this.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
+                        panel.getCurrent().getPassengers().size() == 0){//checks if the train is full or if the station is
+                    if(!(panel.getNext() instanceof Terminal)){
+                        if(panel.getNext().getTrains() == null){            //empty
+                            //move train and notify the train in the previous station
+
+                        }else {
+                            try {
+                                this.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
                         }
+                    }else if(panel.getNext() instanceof Terminal){
+                        //move train to terminal
                     }
 
                 }else{
